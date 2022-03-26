6a4327498930e16589783ed02392687142d48793
Garrett Spears <garrettspears12@gmail.com>
2022-03-25 21:06:05 -0400

    Added text fields to the Login screen.

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React, { useState } from 'react';
+import { Button, View, TextInput } from 'react-native';
 
 function Login(props)
 {
+  // These will retrieve the user and pass to send to the api
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  function doLogin()
+  {
+    var obj = {Login:username, Password:password};
+    var js = JSON.stringify(obj);
+
+    props.appNavigator.navigate("Home");
+  }
+
    return(
     <View id="loginView">
-      <Button title="Login" onPress={() => props.appNavigator.navigate("Home")}/>
+      <TextInput 
+        placeholder="Username" 
+        onChangeText={(text) => setUsername(text)} 
+      />
+      <TextInput 
+        placeholder="Password" 
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)} 
+      />
+      <Button title="Login" onPress={() => doLogin()}/>
     </View>
    );
 };
