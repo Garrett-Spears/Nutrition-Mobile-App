5acbf71a603dd60df4f248a974553c3c51d5ad31
Garrett Spears <garrettspears12@gmail.com>
2022-03-29 16:41:31 -0400

    Got full login functionality working on the app.

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import { Button, View, TextInput } from 'react-native';
+import { Button, View, Text, TextInput } from 'react-native';
+
+global.userId = -1;
+global.firstName = "";
+global.lastName = "";
 
 function Login(props)
 {
@@ -7,12 +11,45 @@ function Login(props)
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
 
-  function doLogin()
+  // Message that will be displayed if login attempt was unsuccessful
+  const [ message, setMessage ] = useState("");
+
+  async function doLogin()
   {
-    var obj = {Login:username, Password:password};
+    var obj = {Login:username.trim(), Password:password.trim()};
     var js = JSON.stringify(obj);
 
-    props.appNavigator.navigate("Home");
+    try
+    {
+      // Make an api call to the remote login api (No local login api defined for mobile app)
+      var bp = require("./Path.js");
+      const response = await fetch(bp.buildPath("api/login"),{method:'POST', body:js, headers:{'Content-Type': 'application/json'}});
+      var res = JSON.parse(await response.text());
+      
+      if( res.UserId <= 0 )
+      {
+          // Unsucessful login attempt
+          setMessage("The user/password combination you entered is incorrect. Please try again.");
+      }
+      else
+      {          
+          // Store the user's info as global variables
+          global.firstName = res.FirstName;
+          global.lastName = res.lastName;
+          global.userId = res.UserId;
+
+          // Clear any possible login error message
+          setMessage("");
+
+          // Route the user to the home page of the app
+          props.appNavigator.navigate("Home");
+      }
+    }
+    catch(e)
+    {
+      setMessage(e.toString());
+      return;
+    }
   }
 
    return(
@@ -27,6 +64,7 @@ function Login(props)
         onChangeText={(text) => setPassword(text)} 
       />
       <Button title="Login" onPress={() => doLogin()}/>
+      <Text>{message}</Text>
     </View>
    );
 };
